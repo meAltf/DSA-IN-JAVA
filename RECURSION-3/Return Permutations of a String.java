87ce335d9fa5b128b8630708121dd1cf52d73a19
ALATAF ANSARI <alataf.dce.cs1@gmail.com>
2023-01-25 16:06:51 +0530

    Create Return Permutations of a String.java

---
+++ RECURSION-3/Return Permutations of a String.java
@@ -0,0 +1,36 @@
+package Recursion3;
+
+public class returnPermutationOfString {
+
+	
+	public static String[] permutationOfString(String input){
+		// Write your code here
+		if (input.length() == 0) {
+			return new String[1];
+		}
+		if (input.length() == 1) {
+			String[] arr = new String[1];
+			arr[0] = input;
+			return arr;
+
+		}
+		char c = input.charAt(0);
+		String[] temp = permutationOfString(input.substring(1));
+		String[] output = new String[temp.length * input.length()];
+		int k = 0;
+		for (int i = 0; i < temp.length; i++) {
+			String newstr = temp[i];
+			for (int j = 0; j < newstr.length(); j++) {
+				output[k] = newstr.substring(0, j) + c + newstr.substring(j);
+				k = k + 1;
+			}
+			output[k] = newstr + c;
+			k = k + 1;
+
+		}
+		return output;
+		
+	}
+	
+}
+
