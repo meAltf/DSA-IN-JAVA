f4155c71677f921f7d4cb9fb0c13f70c4bb00c94
ALATAF ANSARI <alataf.dce.cs1@gmail.com>
2023-02-01 11:41:45 +0530

    Create Find Character Case.java

---
+++ Conditionals and Loops/Find Character Case.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Solution {
+    
+    public static void main(String[] args) {
+        
+        Scanner s = new Scanner(System.in);
+        String str = s.nextLine();
+        char ch = str.charAt(0);
+
+        if(ch>='A' && ch<='Z'){
+            System.out.println(1);
+        }
+
+        else if(ch>='a' && ch<='z'){
+            System.out.println(0);
+        }
+        
+        else{
+            System.out.println(-1);
+        }
+
+
+
+    }
+}
