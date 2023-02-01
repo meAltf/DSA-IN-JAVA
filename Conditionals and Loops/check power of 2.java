0d65a5098078ded7ea8b05c2a87479c12dfa6782
ALATAF ANSARI <alataf.dce.cs1@gmail.com>
2023-02-01 13:42:59 +0530

    Create check power of 2.java

---
+++ Conditionals and Loops/check power of 2.java
@@ -0,0 +1,16 @@
+public static boolean isPowerTwo(long n){
+  
+    //left most bit should be one only
+    
+  return (n!=0 && (n&(n-1)==0);
+  
+};
+          
+/********************************************
+
+n == 0 false;
+
+01000 & 0111 == 0, power of two else not
+
+
+********************************************/
