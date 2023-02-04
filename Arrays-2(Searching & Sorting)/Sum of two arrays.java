a4cbef871e8b626704251b7f387f71de0d05470f
ALATAF ANSARI <alataf.dce.cs1@gmail.com>
2023-02-04 19:14:21 +0530

    Create Sum of two arrays.java

---
+++ Arrays-2(Searching & Sorting)/Sum of two arrays.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+public class Solution {
+
+    public static void sumOfTwoArrays(int arr1[], int arr2[], int output[]) {
+
+        int m = arr1.length-1;
+        int n = arr2.length-1;
+        int c = 0;
+        
+        int x = Math.max(m,n) + 1;        
+        
+        while(m >= 0 && n>=0){
+            int sum = arr1[m] + arr2[n] + c;
+            output[x] = sum%10;
+            c = sum/10;
+            m--;
+            n--;
+            x--;
+        }
+        
+        while(m >= 0){
+            int sum = arr1[m] + c;
+            output[x] = sum%10;
+            c = sum/10;
+            m--;
+            x--;
+        }
+        while(n >= 0){
+            int sum = arr2[n] + c;
+            output[x] = sum%10;
+            c = sum/10;
+            n--;
+            x--;
+        }
+        output[0] = c;
+
+    }
+
+}
