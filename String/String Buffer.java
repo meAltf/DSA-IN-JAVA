2b77ee2a555d27f624ed1fb43a2b9d5aedca877a
ALATAF ANSARI <alataf.dce.cs1@gmail.com>
2023-02-06 22:51:21 +0530

    Create String Buffer.java

---
+++ String/String Buffer.java
@@ -0,0 +1,11 @@
+package string;
+import java.util.Scanner;
+//public class StringBuffer {
+
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		StringBuffer str = new StringBuffer("abc");
+//		str.setcharAt(0,'d');
+//	}
+//
+//}
