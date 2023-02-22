1b1185a38603ca1e6a4d73dfa8ad368fa952a6e3
ALATAF ANSARI <alataf.dce.cs1@gmail.com>
2023-02-22 13:44:21 +0530

    Create Sum of Digits.java

---
+++ RECURSION_ASSIGNMENT/Sum of Digits.java
@@ -0,0 +1,24 @@
+package recursion_Assignment;
+
+public class SumOfDigits {
+
+	
+	public static int sumOfDigits(int input){
+		// Write your code here
+
+		if(input ==0){
+			return 0;
+		}
+		int sum =0;
+		int smallOutput = input%10 + sumOfDigits(input/10);
+		sum = sum + smallOutput;
+		return sum;
+
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
