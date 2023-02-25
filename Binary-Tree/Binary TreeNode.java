5dff15da8b2e241a2ca58806e36b8217b29b2ee8
ALATAF ANSARI <alataf.dce.cs1@gmail.com>
2023-02-25 20:37:23 +0530

    Create Binary TreeNode.java

---
+++ Binary-Tree/Binary TreeNode.java
@@ -0,0 +1,13 @@
+package binaryTrees;
+
+public class BinaryTreeNode<T> {
+		
+	public T data;
+	public BinaryTreeNode<T>left;
+	public BinaryTreeNode<T>right;
+	
+	public BinaryTreeNode(T data) {
+		this.data= data;
+	}
+	
+}
