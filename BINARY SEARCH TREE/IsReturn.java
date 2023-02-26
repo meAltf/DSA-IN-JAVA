c33e5832290276d02d50c43c491940149588f125
ALATAF ANSARI <alataf.dce.cs1@gmail.com>
2023-02-26 12:46:21 +0530

    Create IsReturn.java

---
+++ BINARY SEARCH TREE/IsReturn.java
@@ -0,0 +1,15 @@
+package binarySearchTree;
+
+public class IsBSTReturn {
+	int min;
+	int max;
+	boolean isBST;
+	
+//create constructor for this
+
+public IsBSTReturn(int min, int max, boolean isBST) {
+	this.min = min;
+	this.max = max;
+	this.isBST = isBST;
+    }
+}
