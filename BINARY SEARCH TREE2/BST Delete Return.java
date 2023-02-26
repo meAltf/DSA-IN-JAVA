3f7db2b1fdead090bda84e44c832dd94c4be5afe
ALATAF ANSARI <alataf.dce.cs1@gmail.com>
2023-02-26 13:04:06 +0530

    Create BST Delete Return.java

---
+++ BINARY SEARCH TREE2/BST Delete Return.java
@@ -0,0 +1,16 @@
+package binarySearchTree2;
+
+import binaryTrees.BinaryTreeNode;
+
+public class BSTDeleteReturn {
+
+	BinaryTreeNode<Integer> root;
+	boolean deleted;
+
+
+//create constructor to set data always 
+public BSTDeleteReturn(BinaryTreeNode<Integer> root, boolean deleted) {
+	this.root= root;
+	this.deleted= deleted;
+	}
+}
