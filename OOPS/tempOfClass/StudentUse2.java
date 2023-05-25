17a76bd0a7677eae4c735fab3dc04a2e4aac5310
ALATAF ANSARI <alataf.dce.cs1@gmail.com>
2023-05-25 13:10:12 +0530

    Create StudentUse2.java

---
+++ OOPS/tempOfClass/StudentUse2.java
@@ -0,0 +1,15 @@
+package tempOfClass;
+
+import classesandobjects.Student;
+
+public class StudentUse2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Student s1 = new Student();
+		System.out.println(s1.name);
+
+	}
+
+}
