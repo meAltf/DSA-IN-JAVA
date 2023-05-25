a411248b1e47a2c9ae027235caebd55b75106d1d
ALATAF ANSARI <alataf.dce.cs1@gmail.com>
2023-05-25 12:53:48 +0530

    Create Polynomial.java

---
+++ OOPS/Classes and Objects/Polynomial.java
@@ -0,0 +1,38 @@
+package classesandobjects;
+
+public class Polynomial {
+
+	DynamicArray coefficients;
+	
+	public Polynomial() {
+		
+	public void setCoefficient(int deg, int coeff) {
+		
+	public int getCoefficient(int deg) {
+		
+	public int degree() {
+		
+	public void print() {
+		
+	public void add(Polynomial p) {
+		
+	public void substract(Polynomial p) {
+		
+	public void multiply(Polynomial p) {
+		
+	public int evaluate(int x) {
+		
+	public static Polynomial add(Polynomial p1, Polynomial p2) {
+	
+		
+	}
+	}
+	}
+	}
+	}
+	}
+	}
+	}
+		}
+	}
+}
