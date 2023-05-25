09a63531ed6e1e571d36ef3e23d39c2ddd5b8294
ALATAF ANSARI <alataf.dce.cs1@gmail.com>
2023-05-25 13:04:27 +0530

    Create ExceptionDemo.java

---
+++ OOPS/Exceptions/ExceptionDemo.java
@@ -0,0 +1,21 @@
+//package exceptions;
+//
+//public class ExceptionDemo {
+//	
+//	public static int fact(int n ) {
+//		
+//		public static int divide(int a, int b) {
+//			if(b==0) {
+//				throw new ArithmeticException();
+//			}
+//			return a/b;
+//		}
+//	}
+//
+//	public static void main(String[] args) {
+//		
+//		
+//
+//	}
+//
+//}
