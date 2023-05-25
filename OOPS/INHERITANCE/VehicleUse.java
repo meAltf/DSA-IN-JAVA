51e66dd36f968540878fb0a146149e3ce9626c3b
ALATAF ANSARI <alataf.dce.cs1@gmail.com>
2023-05-25 12:59:53 +0530

    Create VehicleUse.java

---
+++ OOPS/INHERITANCE/VehicleUse.java
@@ -0,0 +1,44 @@
+package inheritance;
+
+public class VehicleUse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		//
+		
+		/*
+//		Vehicles v = new Vehicles();
+		Vehicles v = new Vehicles(100);
+		
+		//Car c = new Car();
+		
+		Car c =  new Car(10);
+		c.print();
+		
+		/*
+		Vehicles v = new Vehicles();
+		
+		//v.color = "Red";
+		v.setColor("Red");
+		v.maxSpeed = 85;
+		v.print();
+		
+		Car c = new Car();
+		//c.color = "Black";
+		c.setColor("Black");
+		c.maxSpeed= 152;
+		c.numDoors = 4;
+		c.printMaxSpeed();
+		c.print();
+		//c.print();
+		//c.printCar();
+		c.print();
+		
+		
+		Bicycle b = new Bicycle();
+		b.print(); 
+*/
+	}
+
+}
